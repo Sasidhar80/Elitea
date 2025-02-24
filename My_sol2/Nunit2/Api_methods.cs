20e6c99663e61b1bc9dd98c7a825ffdd908ecf20
Sasidhar80 <sasidhar692@gmail.com>
2025-02-24 15:16:40 +0530

    Update Api_methods.cs
    Api_Methods

--- My_sol2/Nunit2/Api_methods.cs
+++ My_sol2/Nunit2/Api_methods.cs
@@ -1,12 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using RestSharp;
+using System;
 using System.Threading.Tasks;
 
-namespace Nunit2
+public class ApiMethods
 {
-    internal class Api_methods
+    private readonly RestClient _client;
+
+    public ApiMethods(string baseUrl)
     {
+        _client = new RestClient(baseUrl);
+    }
+
+    public async Task<IRestResponse> GetAsync(string resource, object queryParams = null)
+    {
+        var request = new RestRequest(resource, Method.GET);
+        if (queryParams != null)
+        {
+            foreach (var param in queryParams.GetType().GetProperties())
+            {
+                request.AddQueryParameter(param.Name, param.GetValue(queryParams)?.ToString());
+            }
+        }
+        return await _client.ExecuteAsync(request);
+    }
+
+    public async Task<IRestResponse> PostAsync(string resource, object body = null)
+    {
+        var request = new RestRequest(resource, Method.POST);
+        if (body != null)
+        {
+            request.AddJsonBody(body);
+        }
+        return await _client.ExecuteAsync(request);
+    }
+
+    public async Task<IRestResponse> PutAsync(string resource, object body = null)
+    {
+        var request = new RestRequest(resource, Method.PUT);
+        if (body != null)
+        {
+            request.AddJsonBody(body);
+        }
+        return await _client.ExecuteAsync(request);
+    }
+
+    public async Task<IRestResponse> PatchAsync(string resource, object body = null)
+    {
+        var request = new RestRequest(resource, Method.PATCH);
+        if (body != null)
+        {
+            request.AddJsonBody(body);
+        }
+        return await _client.ExecuteAsync(request);
+    }
+
+    public async Task<IRestResponse> DeleteAsync(string resource, object queryParams = null)
+    {
+        var request = new RestRequest(resource, Method.DELETE);
+        if (queryParams != null)
+        {
+            foreach (var param in queryParams.GetType().GetProperties())
+            {
+                request.AddQueryParameter(param.Name, param.GetValue(queryParams)?.ToString());
+            }
+        }
+        return await _client.ExecuteAsync(request);
     }
 }
